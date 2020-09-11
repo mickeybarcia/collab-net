5add232b61f801833327535fa0ff2c12582e520c
Michaela <mickeydbarcia@gmail.com>
2020-09-10 23:10:47 -0400

    add restart step

--- refresh_data.sh
+++ refresh_data.sh
@@ -1,2 +1,3 @@
 python data/charts_scrape.py 
 python data/build_net.py 
+curl -n -X DELETE https://api.heroku.com/apps/collab-net/dynos \n -H "Content-Type: application/json" \n -H "Accept: application/vnd.heroku+json; version=3" \n -H "Authorization: Bearer ${AUTH_TOKEN}"
