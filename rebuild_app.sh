fcab2a4d8a46a0f01741664740e1e1851741228e
Michaela <mickeydbarcia@gmail.com>
2020-09-11 23:13:11 -0400

    update job

--- rebuild_app.sh
+++ rebuild_app.sh
@@ -1,3 +1,1 @@
-python data/charts_scrape.py 
-python data/build_net.py 
 curl -n -X DELETE https://api.heroku.com/apps/collab-net/dynos \n -H "Content-Type: application/json" \n -H "Accept: application/vnd.heroku+json; version=3" \n -H "Authorization: Bearer ${AUTH_TOKEN}"
---
+++ update_data.sh
@@ -0,0 +1,2 @@
+python data/charts_scrape.py 
+python data/build_net.py 
